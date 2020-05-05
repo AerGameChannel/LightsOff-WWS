b0d66ce5658e68d250d77e65ff2f228a1d7c6ae8
AerGameChannel <63594377+AerGameChannel@users.noreply.github.com>
2020-05-05 15:10:01 +0300

    Add files via upload

---
+++ LightsOff-WWS/EventHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using WW_SYSTEM;
+using WW_SYSTEM.EventHandlers;
+using WW_SYSTEM.Events;
+
+namespace LightsOff
+{
+    class EventHandler: IEventHandlerAdminQuery
+    {
+        public Plugin plugin;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+        public void OnAdminQuery(AdminQueryEvent ev)
+        {
+            string[] array = ev.Query.Split(new char[]
+            {
+                    ' '
+            });
+            if (ev.Query.ToLower().StartsWith("lo") || ev.Query.ToLower().StartsWith("lights"))
+            {
+                if (array.Length <= 1)
+                {
+                    ev.Output = "LightsOff#Usage: lo/lights <time in seconds>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (string.IsNullOrEmpty(array[1]))
+                {
+                    ev.Output = "LightsOff#Usage: lo/lights <time in seconds>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (array.Length > 1)
+                {
+                    try
+                    {
+                        if (!ev.Admin.IsPermitted(PlayerPermissions.FacilityManagement))
+                        {
+                            ev.Output = "LightsOff#Not enough permissions";
+                            ev.Successful = false;
+                            ev.Handled = true;
+                            return;
+                        }
+                        if (array.Length > 1)
+                        {
+                            if (array[1].ToLower().Contains("help"))
+                            {
+                                ev.Output = "LightsOff#Usage: lo <time in seconds>";
+                                ev.Successful = true;
+                                ev.Handled = true;
+                                return;
+                            }
+                            else
+                            {
+                                int lotime = int.Parse(array[1]);
+
+                                Server.Round.EnableFlickering(lotime, false);
+
+                                ev.Output = "LightsOff#Turned off the lights ";
+                                ev.Successful = true;
+                                ev.Handled = true;
+                                return;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ev.Output = "LightsOff#Error: " + ex;
+                        ev.Successful = false;
+                        ev.Handled = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# LightsOff-WWS
+An WW_SYSTEM plugin for SCP:SL, that allows you to turn off lights in the facility for N time in seconds
+
+# Configs
+| Option | Type | Default Value | Description |
+| --- | --- | --- | --- |
+| lightsoff_enable | Bool | true | Enables/Disables the plugin |
+
+# Commands
+| Command | Description |
+| --- | --- |
+| lights | Shows usage of command |
+| lo | Shows usage of command |
+| lights help | Shows usage of command |
+| lo help | Shows usage of command |
+| lights [time in seconds] | Disables lights in the facility for N time (in seconds) |
+| lo [time in seconds] | Disables lights in the facility for N time (in seconds) |
